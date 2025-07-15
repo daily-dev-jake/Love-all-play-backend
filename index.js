af9888d23230a38cd578cfc0b2c9a78d16d65c22
daily-dev-jake <jlele92@outlook.com>
2025-07-15 15:09:04 +0800

    fix typo

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ mongoose.connect(process.env.MDB_URL, { useNewUrlParser: true });
 // app.use(cors()); // Allows All origins
 // Allows only our dev localhost (MORE CONTROL)
 const corsAllowedClient = process.env.NODE_ENV === 'production'
-  ? [`${process.env.FRONTEND_ORIGIN}`]
-  : [`http://localhost:${process.env.CLIENT_PORT}`];
+  ? `${process.env.FRONTEND_ORIGIN}`
+  : `http://localhost:${process.env.CLIENT_PORT}`;
 
 app.use(cors({
   origin: corsAllowedClient,
